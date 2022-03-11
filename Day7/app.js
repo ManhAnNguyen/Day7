5447a75cfdd885bead924c1d00c7ae4d1b361b88
ManhAnNguyen <thungantn1999@gmail.com>
2022-03-12 00:00:00 +0700

    add Day7

---
+++ Day7/app.js
@@ -0,0 +1,112 @@
+//number
+//string : 'yêu em', 'chúc ngủ ngon'
+//boolean : true và false
+//undefine : khai báo ra mà k gán giá trí,giá trí không xác định
+//Null   : không có giá trị thì sẽ là null
+
+// var
+// var a = "12345";
+// console.log(a);
+// var b = 12345;
+// console.log(b);
+
+// var c;
+// console.log(c);
+
+// var number = 123;
+// var number = false;
+// console.log(number);
+
+//let
+
+// let number = 3;
+
+// let number = "mindX";
+
+// console.log(number);
+
+//const
+
+// const number = 3;
+// const number = "bye world";
+// console.log(number);
+
+//hoisting
+
+// var a;
+
+// console.log(a);
+
+// var a = 3;
+
+// console.log(b);
+
+// let b = 4;
+
+// console.log(c);
+
+// const c = 5;
+
+// methods
+
+// Nunber
+
+//to string
+
+// let number = 1234;
+// console.log(number.toString());
+
+// let string = "12345";
+
+// console.log(parseInt(string));
+
+//string
+// const string = "      Hello World         ";
+// const string2 = "Bye World";
+// const string3 = string.concat(" ", string2);
+
+// console.log(string.trim());
+
+// const number = 2;
+
+// console.log("3" == 3);
+
+// console.log(3 > 2 > 1); //true > 1 --> 1 > 1 --> false
+// console.log(1 < 2 < 3); //true < 3 -> 1 < 3 -> true
+
+//operator
+
+// +
+// const number1 = 1;
+// const number2 = 2;
+// const number3 = number1 + number2;
+
+// let number4 = 4;
+// let number5 = 5;
+
+// let number6 = number4 + number5;
+
+// console.log(number6);
+
+// console.log(number4);
+
+// -
+
+// let number1 = 4;
+// let number2 = 1;
+// let number3 = --number1 - --number2;
+
+// console.log(number3);
+
+// console.log(number1);
+
+// *, / < %
+
+// console.log(1000001 % 3);
+//prompt và alert
+
+const value = prompt("Enter your name?");
+
+console.log(value);
+
+document.write(`Hello ${value}`);
---
+++ Day7/readme.md
@@ -0,0 +1,22 @@
+- Kiểu dữ liệu :Number,String,Boolean,Undefine,Null và Object(array,funcion)
+
+5 kiểu dữ liệu đầu là kiểu dữ liệu nguyên thuỷ(primitive)
+
+- Khai báo biến : var ,const,let
+
+  -Var : có thể đổi giá trị và khai báo lại được
+  -Let : có thể đổi giá trị nhưng không khai báo lại được
+  -Const : không thay đổi được giá trị không khai báo được
+  -Hoisting là gì? cách mà javascript đưa phần khai báo biến lên đầu , var : undefined ,const và let sẽ lỗi
+
+  - == va ===
+    == (không bắt buộc) : chỉ so sánh giá trị
+    === (bắt buộc) : so sánh cả giá trị và kiểu dữ liệu
+
+    1 = true
+    0 = false
+
+- Toán tử : + - \* / %
+
+  % chia hết cho 2 === 0;
+  không chia hết cho 2 === 1;
